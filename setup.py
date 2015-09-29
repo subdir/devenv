bfcc93af0bc62c0cef57cad110ddb8cc559ec660
akimov <akimov@yandex-team.ru>
2015-09-29 21:48:17 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='dockerenv',
+    version='1.0',
+    description='Utilities for developing, testing, building and deploying code using docker',
+    author='Alexey Akimov',
+    url='https://github.com/subdir/dockerenv',
+    packages=['dockerenv'],
+)
+
