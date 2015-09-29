c7b59b90243e6755988ce05a8faf6dfa1d744c9e
akimov <akimov@yandex-team.ru>
2015-09-29 12:37:24 +0300

    - split module - pylintified

--- hostuser.sh
+++ hostuser.sh
@@ -7,9 +7,6 @@ set -o pipefail
 : ${TARGET_UID?}
 : ${TARGET_GID?}
 
-userdel "${TARGET_USER}" || true
-groupdel "${TARGET_USER}" || true
-
 groupadd "${TARGET_USER}" --gid "${TARGET_GID}"
 useradd "${TARGET_USER}" \
     --uid "${TARGET_UID}" \
@@ -17,15 +14,11 @@ useradd "${TARGET_USER}" \
     --groups sudo \
     --password "$(perl -e'print crypt("user", "aa")')"
 
-if [[ -x /usr/bin/chpst ]]; then
-    sudo_cmd="chpst -u user:user"
-elif [[ -x /usr/bin/sudo ]]; then
-    sudo_cmd="sudo -u user -g user"
+if [[ $# != 0 ]]; then
+    chpst -u user:user "$@"
+else
+    chpst -u user:user bash -i
 fi
 
-if [[ $# != 0 ]]; then
-    exec $sudo_cmd "$@"
-else
-    exec $sudo_cmd bash -i
-fi
+userdel "${TARGET_USER}"
 
