931025d5d2000cdbf288b6d42cdd2391acdcd9ec
subdir <subdir@gmail.com>
2015-10-01 15:27:49 +0300

    base_image moved to config files

--- dockerenv/__init__.py
+++ dockerenv/__init__.py
@@ -17,6 +17,48 @@ from dockerenv.image_cache import stored_cache, CachedSnapshotter
 from dockerenv.utils import resource
 
 
+class Script(object):
+    def __init__(self, fpath, dev):
+        self.fpath = fpath
+        self.dev = dev
+
+
+class Config(object):
+    def __init__(self, base_image, scripts):
+        self.base_image = base_image
+        self.scripts = scripts
+
+    @classmethod
+    def from_file(cls, fname):
+        config_dir = os.path.dirname(fname)
+        with open(fname) as fobj:
+            config = json.load(fobj)
+        scripts = []
+        for rec in config['scripts']:
+            scripts.append(Script(
+                fpath = os.path.join(config_dir, rec['script']),
+                dev = bool(rec.get('dev')),
+            ))
+        return cls(config["base_image"], scripts)
+
+
+def make_snapshotters(scripts, base_dir, base_image, cache):
+    runner = HostUserRunner(allow_sudo=True)
+    for script in scripts:
+        if script.dev:
+            yield DevelopSnapshotter([script.fpath], base_dir, runner)
+        else:
+            yield CachedSnapshotter(
+                wrapped(BuildInstallSnapshotter(
+                    cmd_prefix = ["./" + os.path.basename(script.fpath)],
+                    context = Context({os.path.basename(script.fpath): script.fpath}),
+                    build_runner = runner.with_image(base_image),
+                    install_runner = runner,
+                )),
+                cache,
+            )
+
+
 def wrapped(snapshotter):
     wrapper = resource('debian_cleanup_wrapper.sh')
     snapshotter.context.add('wrapper.sh', wrapper)
@@ -25,33 +67,9 @@ def wrapped(snapshotter):
     return snapshotter
 
 
-def parse_config(fname, base_dir, base_image, cache):
-    runner = HostUserRunner(allow_sudo=True)
-    config_dir = os.path.dirname(fname)
-    with open(fname) as fobj:
-        config = json.load(fobj)
-
-    for rec in config:
-        # TODO: support config['context']
-        script_path = os.path.join(config_dir, rec['script'])
-        if rec.get('dev'):
-            yield DevelopSnapshotter([script_path], base_dir, runner)
-        else:
-            yield CachedSnapshotter(
-                wrapped(BuildInstallSnapshotter(
-                    cmd_prefix = ["./" + os.path.basename(script_path)],
-                    context = Context({os.path.basename(script_path): script_path}),
-                    build_runner = runner.with_image(base_image),
-                    install_runner = runner,
-                )),
-                cache,
-            )
-
-
 def main(
     target_configs,
     base_dir = None,
-    base_image = 'ubuntu:latest',
     image_cache_fname = None,
 ):
     argparser = argparse.ArgumentParser()
@@ -66,6 +84,8 @@ def main(
     base_dir = base_dir or os.path.abspath(os.path.dirname(sys.argv[0]))
     image_cache_fname = image_cache_fname or os.path.join(base_dir, 'docker_image_cache.json')
 
+    config = Config.from_file(target_configs[args.target])
+
     with stored_cache(image_cache_fname) as image_cache:        
         init_snapshotter = CachedSnapshotter(
             cache = image_cache,
@@ -75,12 +95,12 @@ def main(
                 runner = Runner(entrypoint='/bin/sh'),
             ),
         )
-        init_image = init_snapshotter(base_image)
+        init_image = init_snapshotter(config.base_image)
         snapshotter = CompoundSnapshotter(
-            parse_config(target_configs[args.target], base_dir, init_image, image_cache)
+            make_snapshotters(config.scripts, base_dir, init_image, image_cache)
         )
-
         image = snapshotter(init_image)
+
         if not args.build_only:
             runner = HostUserRunner(allow_sudo=True, home_volume=base_dir)
             runner = runner.with_volumes([Volume(base_dir, base_dir, 'rw')])
