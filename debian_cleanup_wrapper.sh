7af18905373f3c6eb9b59ce78c002f95b4ef82d7
Alexey Akimov <akimov@yandex-team.ru>
2015-10-02 17:19:12 +0300

    use find -delete

--- debian_cleanup_wrapper.sh
+++ debian_cleanup_wrapper.sh
@@ -9,6 +9,6 @@ if [[ "${!#}" == "install" ]]; then
     sudo apt-get autoremove --yes
     sudo apt-get purge --yes
     sudo apt-get clean --yes
-    (sudo find /var/lib/apt/lists/ /tmp/ -mount -mindepth 1 -maxdepth 1 -print0 | sudo xargs -0 -r rm -rf --one-file-system) || true
+    sudo find /var/lib/apt/lists/ /tmp/ -mount -mindepth 1 -maxdepth 1 -delete | true
 fi
 
